41d4e3d80c40914e6d6693cc2689976d79964203
Carlos Manente <cmanente89@gmail.com>
2025-01-16 18:06:18 -0300

    modifique data e item para agregar + imagenes

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Item = ({ product }) => {
     return (
         <Link to={"/detail/"+product.id} className="item">
-            <img src={product.image} className="img-item" alt="" />
+            <img src={product.image[0]} className="img-item" alt="" />
             <p className="text-item">{product.name}</p>
             <p className="text-item">${product.price}</p>
 
--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ const products = [
         name: "Patagonia Express",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
         price: 3000,
-        image: "/img/patagonia-1.jpg",
+        image: ["/img/patagonia-1-1.jpg", "/img/patagonia-1-2.png", "/img/patagonia-1-3.png", "/img/patagonia-1-4.png" ],
         category: "patagonia"
     },
     {
@@ -12,7 +12,7 @@ const products = [
         name: "Patagonia Premium",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
         price: 6000,
-        image: "/img/patagonia-2.jpg",
+        image: ["/img/patagonia-2-1.jpg","/img/patagonia-2-2.png", "/img/patagonia-2-3.png", "/img/patagonia-2-4.png" ],
         category: "patagonia"
     },
     {
@@ -20,7 +20,7 @@ const products = [
         name: "Córdoba Express",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
         price: 1500,
-        image: "/img/cordoba-1.jpg",
+        image: ["/img/cordoba-1-1.jpg", "/img/cordoba-1-2.png", "/img/cordoba-1-3.png", "/img/cordoba-1-4.png"],
         category: "cordoba"
     },
     {
@@ -28,7 +28,7 @@ const products = [
         name: "Córdoba Premium",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
         price: 3000,
-        image: "/img/cordoba-2.jpg",
+        image: ["/img/cordoba-2-1.jpg", "/img/cordoba-2-2.png", "/img/cordoba-2-3.png", "/img/cordoba-2-4.png"],
         category: "cordoba"
     },
     {
@@ -36,7 +36,7 @@ const products = [
         name: "Noroeste Express",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
         price: 2000,
-        image: "/img/noroeste-1.jpg",
+        image: ["/img/noroeste-1-1.jpg", "/img/noroeste-1-2.png", "/img/noroeste-1-3.png", "/img/noroeste-1-4.png"],
         category: "noroeste"
     },
     {
@@ -44,11 +44,10 @@ const products = [
         name: "Noroeste Premium",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate at, rem iste quia magnam assumenda. Consequatur voluptas beatae ex dolorum dolor sapiente, ipsa est nobis voluptate, odit odio harum quae.",
         price: 4000,
-        image: "/img/noroeste-2.jpg",
+        image: ["/img/noroeste-2-1.jpg", "/img/noroeste-2-2.png", "/img/noroeste-2-3.png", "/img/noroeste-2-4.png"],
         category: "noroeste"
     },
 ]
-
 const getProducts = () => {
     return new Promise((resolve, reject) => {
         //con esto simulamos un retraso de red, en este caso de 2 segundos, jugar y cambiar
