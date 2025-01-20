d0d7ba112588cb207a1580b7f6e13c909c519bf1
Carlos Manente <cmanente89@gmail.com>
2025-01-20 18:44:31 -0300

    añadí  deleteCartById and deleteCart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,28 +5,32 @@ import { CartContext } from '../../context/CartContext'
 
 const Cart = () => {
 
-    const {cart, totalPrice} = useContext(CartContext)
-  return (
-    <div>
-        <ul>
-            {
-                cart.map( (productCart)=>(
-                    <li key={productCart.id}>
-                        <p>{productCart.name}</p>
-                        <img src={productCart.image[0]} width={100} alt="" />
-                        <p>cantidad: {productCart.quantity}</p>
-                        <p>precio unitario: {productCart.price}</p>
-                        {/* implementar un subtotal!!!! */}
-                    </li> 
-                ))
-            }
-        </ul>
-        {/* se muestra el precio total si no hay nada en 0, ver  de sacarlo!!!! */}
-        
-        <h3>Total: {totalPrice()}</h3>
-    </div>
-    
-  )
+    const { cart, totalPrice, deleteProductById, deleteCart } = useContext(CartContext)
+    return (
+        <div>
+            <ul>
+                {
+                    cart.map((productCart) => (
+                        <li key={productCart.id}>
+                            <p>{productCart.name}</p>
+                            <img src={productCart.image[0]} width={100} alt="" />
+                            <p>cantidad: {productCart.quantity}</p>
+                            <p>precio unitario: {productCart.price}</p>
+                            <p>subtotal: ${productCart.price * productCart.quantity}</p>
+                            <button onClick={() => deleteProductById(productCart.id)}>eliminar</button>
+
+                            {/* implementar un subtotal!!!! */}
+                        </li>
+                    ))
+                }
+            </ul>
+            {/* se muestra el precio total si no hay nada en 0, ver  de sacarlo!!!! */}
+
+            <h3>Total: {totalPrice()}</h3>
+            <button onClick={deleteCart}>vaciar carrito</button>
+        </div>
+
+    )
 }
 
 export default Cart
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -6,31 +6,53 @@ const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
 
     const addProduct = (newProduct) => {
+        const index = cart.findIndex((productCart) => productCart.id === newProduct.id)
+        console.log(index)
+        if (index === -1) {
+            //agrego el producto como nuevo
+            setCart([...cart, newProduct])
 
-        setCart([...cart, newProduct])
+
+        } else {
+            //modifico solamente la cantidad del producto
+            const newCart = [...cart]
+            newCart[index].quantity = newCart[index].quantity + newProduct.quantity
+            //!reformar con sugar syntax
+            setCart(newCart)
+        }
+
+
 
 
 
     }
 
-    const totalQuantity = () =>{
-        const quantity = cart.reduce( (total, productCart) => total + productCart.quantity, 0)
+    const totalQuantity = () => {
+        const quantity = cart.reduce((total, productCart) => total + productCart.quantity, 0)
         return quantity
     }
 
-    const totalPrice = () =>{
-        const price = cart.reduce( (total, productCart) => total + (productCart.quantity * productCart.price ), 0 )
+    const totalPrice = () => {
+        const price = cart.reduce((total, productCart) => total + (productCart.quantity * productCart.price), 0)
         return price
     }
 
 
+    const deleteProductById = (idProduct) => {
+        const filterProducts = cart.filter((productCart) => productCart.id !== idProduct)
+        setCart(filterProducts)
+
+    }
+
+    const deleteCart = () => {
+        setCart([])
+    }
 
 
 
-    console.log(cart)
 
     return (
-        <CartContext.Provider value={{ cart, addProduct, totalQuantity, totalPrice }}>
+        <CartContext.Provider value={{ cart, addProduct, totalQuantity, totalPrice, deleteProductById, deleteCart }}>
             {children}
         </CartContext.Provider>
     )
