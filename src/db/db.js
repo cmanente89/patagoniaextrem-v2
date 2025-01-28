380333d38f92f864036e0efe672e4649c890bcb3
Carlos Manente <cmanente89@gmail.com>
2025-01-28 12:40:12 -0300

    quité la api key del archivo db, la envié a la tutora por chat

--- src/db/db.js
+++ src/db/db.js
@@ -2,14 +2,19 @@ import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBMcZr9jUHVbP4-CRVptUI2P7cfDHuu_FA",
-    authDomain: "patagoniaextrem-9b708.firebaseapp.com",
-    projectId: "patagoniaextrem-9b708",
-    storageBucket: "patagoniaextrem-9b708.firebasestorage.app",
-    messagingSenderId: "1056762619376",
-    appId: "1:1056762619376:web:be019b8ab679abf6522935"
-};
+
+
+
+//!api key enviada por privado!
+
+// const firebaseConfig = {
+//     apiKey: "",
+//     authDomain: "patagoniaextrem-9b708.firebaseapp.com",
+//     projectId: "patagoniaextrem-9b708",
+//     storageBucket: "patagoniaextrem-9b708.firebasestorage.app",
+//     messagingSenderId: "1056762619376",
+//     appId: "1:1056762619376:web:be019b8ab679abf6522935"
+// };
 
 
 const app = initializeApp(firebaseConfig);
